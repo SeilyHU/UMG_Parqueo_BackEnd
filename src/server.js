5bcae557eb4a74254afc83dcbd7715ed20b92cf5
Edy Barillas <barillaserv@gmail.com>
2026-04-01 09:54:15 -0600

    Revert "Feat(b33 c)/crud plan parque"

--- src/server.js
+++ src/server.js
@@ -1,38 +1,10 @@
-/* Cargar variables de entorno PRIMERO */
-require('dotenv').config();
-
-/* Importaciones */
 const app = require('./app');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./swagger');
-const db = require('./config/db'); 
 
-// Documentación Swagger
+// Documetacion.
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.get('/api-docs.json', (req, res) => res.json(swaggerSpec));
 
 const PORT = process.env.PORT || 4000;
-
-async function startServer() {
-    try {
-        await db.initialize();
-        
-        app.listen(PORT, () => {
-
-            console.log(`http://localhost:${PORT}`);
-            console.log(`http://localhost:${PORT}/api-docs`);
-
-        });
-    } catch (error) {
-        console.error(' Error al iniciar el servidor:', error);
-        process.exit(1);
-    }
-}
-
-startServer();
-
-process.on('SIGINT', async () => {
-    console.log('\nCerrando el servidor y conexiones...');
-    await db.close();
-    process.exit(0);
-});
+app.listen(PORT, () => console.log(`Escuchando http://localhost:${PORT}`));
